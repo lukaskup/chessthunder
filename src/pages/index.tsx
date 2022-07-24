3da44354f1572d1c70e195752a36e7ea511684ae
lukaskup <lukasz.skup1999@gmail.com>
2022-07-24 12:36:52 +0200

    feat: quickPlay and gamesList buttons on index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,14 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
+        <div className="grid grid-cols-2 gap-4 h-12 mb-4">
+          <div className="border border-slate-300/20 rounded flex items-center justify-center hover:bg-slate-300/10 ease-in-out duration-300 cursor-pointer">
+            Quick play
+          </div>
+          <div className="border border-slate-300/20 rounded flex items-center justify-center hover:bg-slate-300/10 ease-in-out duration-300 cursor-pointer">
+            Games list
+          </div>
+        </div>
         <QuickPlay />
       </div>
     </>
