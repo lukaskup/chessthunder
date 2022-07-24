4bb238749ee7da4d0a297925b4ed37b6bd570e24
lukaskup <lukasz.skup1999@gmail.com>
2022-07-24 10:00:02 +0200

    feat: nextAuth google provider, firestore adapter, env variables

--- src/server/env-schema.mjs
+++ src/server/env-schema.mjs
@@ -7,4 +7,13 @@ export const envSchema = z.object({
   NEXTAUTH_URL: z.string().url(),
   DISCORD_CLIENT_ID: z.string(),
   DISCORD_CLIENT_SECRET: z.string(),
+  GOOGLE_CLIENT_ID: z.string(),
+  GOOGLE_CLIENT_SECRET: z.string(),
+  FIREBASE_API_KEY: z.string(),
+  FIREBASE_APP_ID: z.string(),
+  FIREBASE_AUTH_DOMAIN: z.string(),
+  FIREBASE_DATABASE_URL: z.string(),
+  FIREBASE_PROJECT_ID: z.string(),
+  FIREBASE_STORAGE_BUCKET: z.string(),
+  FIREBASE_MESSAGING_SENDER_ID: z.string(),
 });
