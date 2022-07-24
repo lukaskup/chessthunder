95205e4cec9d13a7ba925a1f671af33f9e8ade9f
lukaskup <lukasz.skup1999@gmail.com>
2022-07-24 13:14:25 +0200

    feat: GamesList component styling

--- src/components/GamesList/index.tsx
+++ src/components/GamesList/index.tsx
@@ -5,11 +5,31 @@ import { trpc } from "../../utils/trpc";
 export const GamesList: NextPage = () => {
   const { data: games } = trpc.useQuery(["api.gamesList"]);
   return (
-    <div className="border border-slate-300/20 rounded">
-      {games &&
-        games.map((game: any) => {
-          return <div key={game.player}>{game.player}</div>;
-        })}
+    <div className="border border-slate-300/20 rounded px-12 py-6">
+      <table className="table-fixed text-left w-full">
+        <thead className="font-bold border-b border-slate-300/40">
+          <tr>
+            <th>Player</th>
+            <th>Rating</th>
+            <th>Mode</th>
+          </tr>
+        </thead>
+        <tbody>
+          {games &&
+            games.map((game: any) => {
+              return (
+                <tr
+                  key={game.player}
+                  className="border-b border-slate-300/20 h-12 last:border-0 hover:bg-slate-300/10 ease-in-out duration-300 cursor-pointer"
+                >
+                  <td>{game.player}</td>
+                  <td>{game.rating}</td>
+                  <td>{game.mode}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
     </div>
   );
 };
