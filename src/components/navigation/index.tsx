2e130cb8f179fe6b268a88804947e6bfb9b47e21
lukaskup <lukasz.skup1999@gmail.com>
2022-07-24 13:24:27 +0200

    fix: remove focus:ring from account icons

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -72,7 +72,7 @@ export const Navigation: NextPage = () => {
               <div>
                 <button
                   type="button"
-                  className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                  className="bg-gray-800 flex text-sm rounded-full"
                   id="user-menu-button"
                   aria-expanded="false"
                   aria-haspopup="true"
