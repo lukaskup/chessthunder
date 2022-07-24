3dba7cd5cb45db4d42105075ee9ccdcf15d38687
lukaskup <lukasz.skup1999@gmail.com>
2022-07-24 12:45:55 +0200

    feat: quickPlay and gamesList onClick behavior

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useState } from "react";
 import { QuickPlay } from "../components/QuickPlay";
 
 const Home: NextPage = () => {
+  const [activeButton, setActiveButton] = useState<"quickPlay" | "gamesList">(
+    "quickPlay"
+  );
+
   return (
     <>
       <Head>
@@ -11,14 +16,28 @@ const Home: NextPage = () => {
       </Head>
       <div>
         <div className="grid grid-cols-2 gap-4 h-12 mb-4">
-          <div className="border border-slate-300/20 rounded flex items-center justify-center hover:bg-slate-300/10 ease-in-out duration-300 cursor-pointer">
+          <div
+            className={`border border-slate-300/20 rounded flex items-center justify-center hover:bg-slate-300/10 ease-in-out duration-300 cursor-pointer ${
+              activeButton == "quickPlay" ? "bg-slate-300/10" : ""
+            }`}
+            onClick={() => {
+              setActiveButton("quickPlay");
+            }}
+          >
             Quick play
           </div>
-          <div className="border border-slate-300/20 rounded flex items-center justify-center hover:bg-slate-300/10 ease-in-out duration-300 cursor-pointer">
+          <div
+            className={`border border-slate-300/20 rounded flex items-center justify-center hover:bg-slate-300/10 ease-in-out duration-300 cursor-pointer ${
+              activeButton == "gamesList" ? "bg-slate-300/10" : ""
+            }`}
+            onClick={() => {
+              setActiveButton("gamesList");
+            }}
+          >
             Games list
           </div>
         </div>
-        <QuickPlay />
+        {activeButton === "quickPlay" ? <QuickPlay /> : "Games list"}
       </div>
     </>
   );
