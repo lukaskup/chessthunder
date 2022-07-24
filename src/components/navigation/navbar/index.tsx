bf1cddce4d73d9ab75df15547cd853e63ed280fd
lukaskup <lukasz.skup1999@gmail.com>
2022-07-24 11:47:14 +0200

    feat: navigation styling, global container width change

--- src/components/navigation/navbar/index.tsx
+++ src/components/navigation/navbar/index.tsx
@@ -7,7 +7,7 @@ export const Navbar: NextPage = () => {
   let { data: session } = useSession();
   return (
     <div className="border-b dark:border-slate-300/10">
-      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+      <div className="max-w-5xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-16">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             <button
@@ -51,7 +51,9 @@ export const Navbar: NextPage = () => {
           </div>
           <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex-shrink-0 flex items-center">
-              <div className="text-2xl font-semibold">chessthunder</div>
+              <div className="text-2xl font-semibold -translate-y-[3px]">
+                chessthunder
+              </div>
             </div>
             <div className="hidden sm:flex sm:ml-6 items-center">
               <div className="flex space-x-4">
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,7 +14,7 @@ const MyApp: AppType = ({
   return (
     <SessionProvider session={session}>
       <Navbar />
-      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 pt-4">
+      <div className="max-w-5xl mx-auto px-2 sm:px-6 lg:px-8 pt-4">
         <Component {...pageProps} />
       </div>
     </SessionProvider>
