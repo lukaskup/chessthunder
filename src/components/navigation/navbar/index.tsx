ab482513a88b537f3e626055129ace12bc8b9db8
lukaskup <lukasz.skup1999@gmail.com>
2022-07-24 11:41:13 +0200

    fix: navigation border bottom

--- src/components/navigation/navbar/index.tsx
+++ src/components/navigation/navbar/index.tsx
@@ -6,8 +6,8 @@ import AccountIcon from "./../../../assets/icons/AccountIcon.svg";
 export const Navbar: NextPage = () => {
   let { data: session } = useSession();
   return (
-    <div>
-      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 border-b border-slate-900/10 dark:border-slate-300/10">
+    <div className="border-b dark:border-slate-300/10">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-16">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             <button
