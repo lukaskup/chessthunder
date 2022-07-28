df830c9f4c4b9c0ceafb8b6016072659e1208a73
lukaskup <lukasz.skup@allbright.ai>
2022-07-28 22:22:31 +0200

    feat: chessboard page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import type { AppType } from "next/dist/shared/lib/utils";
 import superjson from "superjson";
 import { SessionProvider } from "next-auth/react";
 import "../styles/globals.css";
+import "../styles/chessboard.css";
 import { Navigation } from "../components/Navigation";
 
 const MyApp: AppType = ({
