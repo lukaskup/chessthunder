8edab7da93f7bf4e30ccb67ea59aface5080083c
lukaskup <lukasz.skup1999@gmail.com>
2022-09-24 17:05:32 +0200

    feat: remove firebase from project, postgresql prisma config

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,8 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 import GoogleProvider from "next-auth/providers/google";
-import { FirestoreAdapter } from "@next-auth/firebase-adapter";
-
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { prisma } from "../../../server/db/client";
 import { env } from "../../../server/env.mjs";
+import GithubProvider from "next-auth/providers/github";
 
 export const authOptions: NextAuthOptions = {
   callbacks: {
@@ -16,15 +13,7 @@ export const authOptions: NextAuthOptions = {
       return session;
     },
   },
-  // adapter: FirestoreAdapter({
-  //   apiKey: process.env.FIREBASE_API_KEY,
-  //   appId: process.env.FIREBASE_APP_ID,
-  //   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  //   databaseURL: process.env.FIREBASE_DATABASE_URL,
-  //   projectId: process.env.FIREBASE_PROJECT_ID,
-  //   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  //   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  // }),
+
   providers: [
     DiscordProvider({
       clientId: env.DISCORD_CLIENT_ID,
