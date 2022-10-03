9f3b97c17b3db699c97841e7e4c77e541b454b3e
lukaskup <lukasz.skup1999@gmail.com>
2022-10-03 20:28:55 +0200

    feat(auth): error fix for credentials provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -13,30 +13,15 @@ import { env } from "../../../server/env.mjs";
 const prisma = new PrismaClient();
 
 export const authOptions: NextAuthOptions = {
-  callbacks: {
-    session({ session, user }) {
-      console.log(session, user);
-      if (session.user) {
-        session.user.id = user.id;
-      }
-      return session;
-    },
-  },
   adapter: PrismaAdapter(prisma),
   providers: [
     CredentialsProvider({
-      // The name to display on the sign in form (e.g. "Sign in with...")
       name: "Credentials",
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
         email: { label: "email", type: "text" },
         password: { label: "password", type: "password" },
       },
       async authorize(credentials, req) {
-        // Add logic here to look up the user from the credentials supplied
         if (!!!credentials) return null;
         const user: any = await prisma.user.findFirst({
           where: {
@@ -45,15 +30,10 @@ export const authOptions: NextAuthOptions = {
           },
         });
 
-        console.log(user);
         if (user) {
-          // Any object returned will be saved in `user` property of the JWT
           return user;
         } else {
-          // If you return null then an error will be displayed advising the user to check their details.
           return null;
-
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
         }
       },
     }),
@@ -70,6 +50,9 @@ export const authOptions: NextAuthOptions = {
       clientSecret: env.DISCORD_CLIENT_SECRET,
     }),
   ],
+  session: {
+    strategy: "jwt",
+  },
 };
 
 export default NextAuth(authOptions);
