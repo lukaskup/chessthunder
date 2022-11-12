d602f224c4ed3dd0673957713f7e9c2a989575f6
lukaskup <lukasz.skup1999@gmail.com>
2022-11-12 10:33:05 +0100

    feat: new icon, tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,12 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    maxWidth: {
+      lg: "1536px",
+    },
     colors: {
       main: "rgb(15 23 42)",
+      white: "white",
       "main-dark": "rgb(14 20 37)",
       "slate-300": "rgb(203 213 225)",
     },
